9d7e25007eed558dcd159033da7d66b666dd9044
Francois Daoust <fd@tidoust.net>
2023-10-25 14:41:04 +0200

    Fix clunky rendering (mostly)
    - Properly register an A-Frame component for processing "paint" events attached
    to a plane.
    - Code supports rendering on displays with DPI scales factors higher than 1.
    - Closing the immersive browser now properly closes the app.
    - Remove all menu bars from immersive browser window.
    
    Still somewhat clunky because plane still needs to be associated with an empty
    <canvas> asset (no need to specify any dimensions) under <a-assets>.

--- browser.js
+++ browser.js
@@ -1,11 +1,38 @@
-window.addEventListener('DOMContentLoaded', () => {
-  const img = document.getElementById("f");
-  const plane = document.getElementById("screen");
-  let counter = 0;
-  ipcRenderer.onPaint((_event, image) => {
-    img.src = image.toDataURL();
-    if (document.getElementById("screen").getObject3D) {
-      document.getElementById("screen").getObject3D("mesh").material.map.needsUpdate = true;
+(function (AFRAME) {
+  // The a-frame component refreshes an image used as texture
+  // whenever a new one is received from the main thread.
+  AFRAME.registerComponent('paint-image', {
+    schema: {
+      id: { type: 'string', default: 'content' }
+    },
+
+    init: function () {
+      this.textureLoader = new THREE.TextureLoader();
+      this.pending = false;
+      ipcRenderer.onPaint(async (_event, target, image) => {
+        if (target !== this.data.id || this.pending) {
+          // Paint message is for someone else
+          // or we're already busy re-drawing the texture
+          return;
+        }
+        this.pending = new Promise(resolve => {
+          this.textureLoader.load(
+            image.toDataURL(),
+            texture => {
+              this.el.getObject3D('mesh').material.map = texture;
+              this.el.getObject3D('mesh').material.map.needsUpdate = true;
+              resolve();
+            },
+            null,
+            err => {
+              console.error(err);
+              resolve();
+            }
+          );
+        });
+        await this.pending;
+        this.pending = null;
+      });
     }
   });
-});
+})(AFRAME);
--- main.js
+++ main.js
@@ -1,8 +1,21 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
-// inclusion du chemin du module Node.js au tout début de votre fichier
+const { app, BrowserWindow, screen } = require('electron');
 const path = require('node:path');
 
-let browserWindow; 
+/**
+ * Scale factor of the primary display.
+ */
+let scaleFactor;
+
+/**
+ * Handle to the immersive browser window
+ */
+let browserWindow;
+
+/**
+ * Handle to the window that contains the HTML that we want to render in
+ * the immersive browser.
+ */
+let contentWindow;
 
 const createBrowserWindow = () => {
   browserWindow = new BrowserWindow({
@@ -10,37 +23,61 @@ const createBrowserWindow = () => {
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
-    }
+    },
+    titleBarStyle: 'hidden'
+  });
+  browserWindow.on('closed', _ => {
+    contentWindow.destroy();
+    contentWindow = null;
+    browserWindow = null;
   });
   browserWindow.loadFile('index.html');
 };
 
 const createContentWindow = () => {
-  const win = new BrowserWindow({
+  contentWindow = new BrowserWindow({
     webPreferences: {
       offscreen: true
     },
     show: false,
     width: 800,
-    height: 600,
+    height: 600
   });
 
-  win.webContents.on('paint', (event, dirty, image) => {
-    browserWindow.webContents.send('paint', image);
+  contentWindow.webContents.on('paint', (event, dirty, image) => {
+    let resized = image;
+    if (scaleFactor !== 1) {
+      // Immersive browser window uses the image as texture. The dimensions of
+      // the texture must be the dimensions of the content window (800x600).
+      resized = image.resize({
+        width: Math.floor(image.getSize().width / scaleFactor),
+        height: Math.floor(image.getSize().height / scaleFactor)
+      });
+    }
+    browserWindow.webContents.send('paint', 'content', resized);
   });
-  win.webContents.setFrameRate(10);
-  win.loadFile('content.html');
+  contentWindow.webContents.setFrameRate(10);
+  contentWindow.loadFile('content.html');
 };
 
-
 app.whenReady().then(() => {
+  // Content window is rendered offscreen, but is still attached to a display.
+  // This affects the Dots per Inch (DPI) scale used to render the content.
+  // Let's assume that the window is attached to the primary display.
+  const primaryDisplay = screen.getPrimaryDisplay();
+  scaleFactor = primaryDisplay.scaleFactor;
+
   createBrowserWindow();
   createContentWindow();
   app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createBrowserWindow()
-  })
-})
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createBrowserWindow();
+    }
+  });
+});
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
-})
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
